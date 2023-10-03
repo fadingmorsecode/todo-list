c53f024a1c00ed431dff59c74eecbbca6b4b19b7
Drake Williamson <williamson.drake@outlook.com>
2023-10-03 11:56:02 -0700

    Create static elements and styling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+import './styles.css';
+import logoImage from './assets/tasktigerlogo.svg';
+import plusImage from './assets/plus-circle-svgrepo-com.svg';
 
+const logoContainer = document.querySelector('.logo-img');
+const logoIcon = new Image();
+logoIcon.src = logoImage;
+logoContainer.appendChild(logoIcon);
+
+const plusIconContainer = document.querySelector('.new-task-btn');
+const plusIcon = new Image();
+plusIcon.src = plusImage;
+plusIconContainer.appendChild(plusIcon);
+
+const projectAddBtn = document.querySelector('.project-add-btn');
+const projectPlusIcon = new Image();
+projectPlusIcon.src = plusImage;
+projectAddBtn.appendChild(projectPlusIcon);
